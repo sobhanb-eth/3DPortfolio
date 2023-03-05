011b853b5d54db0c09aca4078dc8ba5b18439279
Sobhan Bahrami <ednqor@inf.elte.hu>
2023-03-05 21:25:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      3D Developer Portfolio
+    </div>
+  )
+}
+
+export default App
